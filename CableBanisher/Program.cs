501c913bcc4049e2e539162bd7cc508416d967da
Seefzi <seefzi@hotmail.com>
2023-10-22 16:44:15 -0500

    Intermission

--- CableBanisher/Program.cs
+++ CableBanisher/Program.cs
@@ -1,10 +1,39 @@
 ﻿using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello");
+        int i;
+        string input = "i";
+        string[] networkDeviceNames = { };
+        NetworkInterface[] networkDevices = NetworkInterface.GetAllNetworkInterfaces();
+
+        for (i = 0; i < networkDevices.Length; i++)
+        {
+            networkDeviceNames[i] = networkDevices[i].Name;
+        }
+        
+        while (input != null && (input.Length == 0 || char.ToLower(input[0]) != 'q'))
+        {
+            Console.Clear();
+
+            switch (char.ToLower(input[0]))
+            {
+                case 's':
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+    }
+
+    static void PrintTable(NetworkInterface[] interfaces)
+    {
 
     }
 }
